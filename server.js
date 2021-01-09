b5622b0737f3bef8cfe91bdb27efe5db2ba98d95
Valerie TV <vtylskivincent@gmail.com>
2021-01-09 09:56:19 +0100

    fix bug by changing emplacement of dotenv config

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
+
+dotenv.config();
+
 const pool = require('./pool');
 
 const port = process.env.PORT || 5000;
 const app = express();
 
-dotenv.config();
 app.use(express.json());
 
-app.get('/api/ludotheque/', (req, res) => {
-  pool.query('SELECT * FROM games', (err, results) => {
+app.get('/api/games', (req, res) => {
+  pool.query('SELECT * FROM game', (err, results) => {
     if (err) {
       res.status(500).json({
         error: err.message,
