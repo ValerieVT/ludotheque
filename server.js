1a4f9827872cf3401c909a7381d4a2ab74adb3b6
Valerie TV <vtylskivincent@gmail.com>
2021-01-09 09:45:30 +0100

    add app.lisen and first GET road

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-dotenv.config();
 const connection = require('./connection');
 
+const port = process.env.PORT || 3000;
 const app = express();
 
+dotenv.config();
 app.use(express.json());
+
+app.get('/api/ludotheque/', (req, res) => {
+  connection.query('SELECT * FROM games', (err, results) => {
+    if (err) {
+      res.status(500).json({
+        error: err.message,
+      });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.listen(port, (err, res) => {
+  if (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  } else {
+    console.log(`Server is listening on port ${process.env.PORT}`);
+  }
+});
