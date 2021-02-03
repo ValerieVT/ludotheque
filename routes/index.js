1a83d6cfe37d733b1549f86f380d21e2169083f8
Valerie TV <vtylskivincent@gmail.com>
2021-02-03 12:44:00 +0100

    added auth road

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const auth = require('./auth');
 const jeux = require('./jeux');
 const themes = require('./themes');
 
 const router = express.Router();
 
+router.use('/auth', auth);
 router.use('/jeux', jeux);
 router.use('/themes', themes);
 
