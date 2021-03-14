4820b475c0219d503cad1bd969c7b45b69eb26f1
Valerie TV <vtylskivincent@gmail.com>
2021-03-14 20:58:45 +0100

    added POST for new game & added GET to search a game by its name

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+// const multer = require('multer');
+// const renamePictureFile = require('./middlewares/rename-picture-file');
 const pool = require('../pool');
 
 const privateKey = process.env.JWT_SECRET;
 const router = express.Router();
+
+// const storage = multer.diskStorage({});
+
+// const upload = multer({
+//   storage,
+// });
 
 const checkIfAuth = async (req, res, next) => {
   const { token } = req.cookies;
@@ -41,6 +49,54 @@ router.post('/themes/:name', async (req, res) => {
       }
       return res.status(201).send('Thème ajouté !');
     });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+});
+
+router.post('/jeux', (req, res) => {
+  pool.query('INSERT INTO game SET ?', [req.body], (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        error: err.message,
+      });
+    }
+    return pool.query('SELECT * FROM game WHERE id = ?', results.insertId, (err2, results2) => {
+      if (err2) {
+        return res.status(500).json({
+          error: err2.message,
+          sql: err2.sql,
+        });
+      }
+      const host = req.get('host');
+      const location = `http://${host}/api/jeux/${results.insertId}`;
+      return res
+        .status(201)
+        .set('Location', location)
+        .json(results2);
+    });
+  });
+});
+
+// router.post('/photos', checkIfAuth, async (req, res) => {
+//   try {
+//     upload();
+//     res.sendStatus(200);
+//   } catch (error) {
+//     return res.status(500).json({
+//       error: error.message,
+//     });
+//   }
+// });
+
+router.get('/jeux/search', async (req, res) => {
+  try {
+    const searchForName = `%${req.query.name}%`;
+    const sql = 'SELECT * FROM game WHERE name LIKE ?';
+    await pool.query(sql, searchForName);
+    return res.sendStatus(200);
   } catch (error) {
     return res.status(500).json({
       error: error.message,
